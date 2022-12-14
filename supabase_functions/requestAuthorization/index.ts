6cee25a5bcf708dbeb39f6a30d59899b9a2e32f6
khaled.gabr <khaled.gabr>
2022-12-13 17:45:54 -0800

    Finalize requestAuthorization Function

--- supabase_functions/requestAuthorization/index.ts
+++ supabase_functions/requestAuthorization/index.ts
@@ -3,37 +3,45 @@ import { OAuth2Client } from "https://deno.land/x/oauth2_client/mod.ts";
 
 import { corsHeaders } from "../_shared/cors.ts";
 
-const oauth2Client = new OAuth2Client({
-  clientId: Deno.env.get("AWEBER_CLIENT_ID")!,
-  clientSecret: Deno.env.get("AWEBER_CLIENT_SECRET")!,
-  authorizationEndpointUri: "https://auth.aweber.com/oauth2",
-  tokenUri: "https://auth.aweber.com/oauth2/token",
-  redirectUri: "http://localhost:8000/oauth2/callback",
-  defaults: {
-    scope: [
-      "account.read",
-      "list.read",
-      "list.write",
-      "subscriber.read",
-      "subscriber.write",
-      "email.read",
-      "email.write",
-      "subscriber.read-extended",
-      "landing-page.read",
-    ],
-  },
-});
-
-
-serve((req: Request) => {
+serve(async (req: Request) => {
   // This is needed to invoke your function from a browser.
   if (req.method === "OPTIONS") {
     return new Response("ok", { headers: corsHeaders });
   }
 
+  // Setup the Outh2 Object
+  const oauth2Client = new OAuth2Client({
+    clientId: Deno.env.get("AWEBER_CLIENT_ID")!,
+    clientSecret: Deno.env.get("AWEBER_CLIENT_SECRET")!,
+    authorizationEndpointUri: "https://auth.aweber.com/oauth2/authorize",
+    tokenUri: "https://auth.aweber.com/oauth2/token",
+    //redirectUri: "http://localhost:3000/callback",  TODO: Review if needed
+    defaults: {
+      scope: [
+        "account.read",
+        "list.read",
+        "list.write",
+        "subscriber.read",
+        "subscriber.write",
+        "email.read",
+        "email.write",
+        "subscriber.read-extended",
+        "landing-page.read",
+      ],
+    },
+  });
+
+  // Generate a random state
+  const state = crypto.randomUUID();
+
+  // Obtain authorization URL
+  const { uri } = await oauth2Client.code.getAuthorizationUri({
+    state,
+    disablePkce: true,
+  });
+
   const data = {
-    success: oauth2Client.config
-    ,
+    redirectUrl: uri,
   };
 
   try {
--- supabase_functions/requestAuthorization/readme.md
+++ supabase_functions/requestAuthorization/readme.md
@@ -1,10 +1,18 @@
-Follow this setup guide to integrate the Deno language server with your editor:
-https://deno.land/manual/getting_started/setup_your_environment
+Resources:
+1) Contains an example of generating and refreshing access token for Aweber in NodeJS
+https://github.com/aweber/public-api-examples/blob/master/node/get-access-token.js
 
+2) Contains an example of deno oauth2 usage
+https://github.com/cmd-johnson/deno-oauth2-client/blob/master/examples/http.ts   
+
+----------------------
 
 To Deploy:
 supabase functions deploy requestAuthorization --project-ref azmzvdpframywobqkjmz --no-verify-jwt
 
-To Set Aweber Client ID and client Secret
+----------------------
+
+To Set Aweber Client ID and client Secret:
 supabase secrets set AWEBER_CLIENT_ID='REPLACE_CLIENT_ID'
 supabase secrets set AWEBER_CLIENT_SECRET='RELACE_CLIENT_SECRET'
+
