bd63021f414af6baec87bea5bcbde92ef5ade44d
khaled.gabr <khaled.gabr>
2022-12-13 16:50:42 -0800

    add ping function and testing deployment

---
+++ supabase_functions/ping/index.ts
@@ -0,0 +1,28 @@
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+import { corsHeaders } from "../_shared/cors.ts";
+
+console.log("Test Ping Function: #001");
+
+serve((req: Request) => {
+  // This is needed to invoke your function from a browser.
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  const data = {
+    success: true,
+  };
+
+  try {
+    return new Response(JSON.stringify(data), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+});
+
---
+++ supabase_functions/ping/readme.md
@@ -0,0 +1,6 @@
+Follow this setup guide to integrate the Deno language server with your editor:
+https://deno.land/manual/getting_started/setup_your_environment
+
+
+To Deploy:
+supabase functions deploy ping --project-ref azmzvdpframywobqkjmz --no-verify-jwt
