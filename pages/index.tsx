8723ec2b1c2f0256010f3310817b63740c01bd59
Matt Schroder <m3schroder@gmail.com>
2022-12-19 07:46:25 -0600

    simplified index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,35 @@
-import {useSession, useSupabaseClient} from '@supabase/auth-helpers-react'
-import Link from 'next/link'
-import {useEffect, useState} from 'react';
-import {TypeAnimation} from 'react-type-animation';
-import {firstToUpper} from "@lib/stringUtils";
-import ThemeToggler from "@ui/ThemeToggler";
-import Image from "next/image"
-
+import {useState} from 'react'
+import ThemeToggler from "@ui/ThemeToggler.tsx";
+import Provider from "@ui/Provider.tsx";
+import {Combobox, Transition} from "@headlessui/react"
+import Details from "@ui/ProviderDetails.tsx";
+import {IntegrationConfig} from "../supabase/_shared/integration.ts";
 
 const Home = () => {
-    const session = useSession()
-    const supabase = useSupabaseClient()
-    const sectionClass= 'min-h-full py-10 flex flex-col  justify-center mx-auto w-full  xl:w-full px-5 py-8 rounded-md snap-center'
+    const [list, setList] = useState(["Aweber", "MailChimp"]);
+    const [query, setQuery] = useState(undefined);
+    const [selected, setSelected] = useState<string>(undefined);
 
+    const handle = {
+        select: (name: string | undefined) => {
+            setSelected(name)
+        }
+    }
     return (
-        <div className={'snap-y w-full snap-always snap-mandatory flex flex-col gap-12 px-5 h-screen overflow-auto'}>
-            <section className={'full center h-screen  flex-col'}>
-                <h3 className={'text-emerald-600 text-5xl'}>Supabase Newsletter</h3>
-                <Link href={"/signin"}>
-                    <button className={"btn mt-20 px-6 py-3 rounded-lg text-2xl font-semibold"}>Signin</button>
-                </Link>
-                <Link href={"/table"}>
-                    <button className={"btn mt-20 px-6 py-3 rounded-lg text-2xl font-semibold"}>Configurations</button>
-                </Link>
+        <div className={'screen center flex-col'}>
+            <section className={"flex-col center mb-12"}>
+                <h3 className={'mb-5 text font-bold text-2xl font-mono'}>Configurations</h3>
+                <input onChange={(event) => setQuery(event.target.value)}
+                       className={"bg-white shadow-sm border-1 border-neutral-200 px-2  focus:outline-none focus:border-neutral-300 flex mb-1 flex-col py-1 rounded-lg  w-full gap-1"}/>
             </section>
+            <div className={"grid grid-cols-3 grid-rows-auto gap-8"}>
+                {list.filter(x => query === undefined ? true : x.includes(query)).map((provider, idx) => (
+                    <Provider key={idx} provider={provider} onSelect={handle.select}/>
+                ))}
+                    <Details selected={selected} open={selected !== undefined} close={() => handle.select(undefined)}/>
+            </div>
             <ThemeToggler cx={"fixed bottom-5 right-5"}/>
         </div>
-)
+    )
 }
-
 export default Home
